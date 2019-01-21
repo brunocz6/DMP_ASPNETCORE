1c9f22a474e9dd6a85072f8a9177909e98701a4f
brunocz6 <bruno.pfohl@seznam.cz>
2019-01-21 21:28:04 +0100

    Odstranění zbytečných akcí controlleru a views ze scaffoldingu

--- webapp/InternetForum/InternetForum/Controllers/HomeController.cs
+++ webapp/InternetForum/InternetForum/Controllers/HomeController.cs
@@ -8,30 +8,11 @@ using InternetForum.Models;
 
 namespace InternetForum.Controllers
 {
-    public class HomeController : Controller
-    {
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-    }
+	public class HomeController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
 }
